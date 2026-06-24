8d4e2e5313f8247aadda09826633eb2a18110227
fiqriridho <fiqri6149@gmail.com>
2026-06-24 12:54:56 +0700

    Tambah Add product

--- contexts/auth-context.tsx
+++ contexts/auth-context.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 /* ──────────────────────────── Config ──────────────────────────── */
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const API_URL = "";
 
 axios.defaults.withCredentials = true;
 
